c1a71d77d390a468371b5444deb81de2cfaf6116
Razvan Gorea <razvan.gorea2@mail.dcu.ie>
2022-10-06 09:10:47 +0100

    CA284 Programs

---
+++ lab3-is-sorted.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void sorted(int argc, char *argv[]);
+
+int main(int argc, char *argv[]){
+    sorted(argc, argv);
+    return 0;
+}
+
+void sorted(int argc, char *argv[]){
+//temp = a[i];
+//a[i] = a[j];
+//a[j] = temp;
+}
