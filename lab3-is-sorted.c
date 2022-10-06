7ff86692a197b1386f9614c91bf58dcaeb7694c1
Razvan Gorea <razvan.gorea2@mail.dcu.ie>
2022-10-06 12:08:21 +0100

    C Program for CA284

--- lab3-is-sorted.c
+++ lab3-is-sorted.c
@@ -2,23 +2,39 @@
 #include <stdlib.h>
 #include <string.h>
 
-
-int *convert(int *arr, int argc, char *argv[]);
+//Function declarations
+int *sort(int *arr, int argc, char *argv[]);
+void printer(int *arr, int argc);
 
 int
 main(int argc, char *argv[])
-{
+{// Function calls
     int arr[argc - 1];
-    convert(arr, argc, argv);
+    sort(arr, argc, argv);
+    printer(arr, argc);
 }
 
-int *
-convert(int *arr, int argc, char *argv[])
+int * // Creates the 'arr' array composed of integer elements
+sort(int *arr, int argc, char *argv[])
 {
     for (int i = 1; i < argc; ++i){
         arr[i] = atoi(argv[i]);
-    }
-return arr;
+                                  } 
+
+    
+    // bubble sort algorithm to sort the array in ascending order
+    int holder;
+    for (int i = 1; i < argc; i++) {     
+        for (int j = i+1; j < argc; j++) {     
+           if(arr[i] > arr[j]) {    
+               holder = arr[i];    
+               arr[i] = arr[j];    
+               arr[j] = holder;}         }
+                                   }
 }
 
-
+//Prints the sorted array
+void printer(int *arr, int argc){
+    for(int i = 1; i < argc; ++i){
+        printf("%d\n", arr[i]);  }
+}
